3b5a5abc89012496c9dfb825765fbe45e6afa0e0
joecus1 <joecusano57@gmail.comm>
2025-11-10 20:26:11 -0500

    Implement train_bpe function

--- cs336_basics/pretokenization_example.py
+++ cs336_basics/pretokenization_example.py
@@ -1,3 +1,4 @@
+from io import BufferedReader
 import os
 from typing import BinaryIO
 import regex as re
@@ -50,28 +51,48 @@ def find_chunk_boundaries(
     return sorted(set(chunk_boundaries))
 
 
-def count_file_pre_tokens(filepath: str) -> dict[tuple[bytes], int]:
-    ## Usage
+def count_chunk_pre_tokens(
+    chunk: str,
+    special_tokens: list[str]
+) -> dict[tuple[bytes, ...], int]:
+    """Given a str, split into pretokens and count them."""
+    token_counts = {}
+
+    pattern = "|".join(re.escape(tok) for tok in special_tokens)
+    cleaned_chunks = re.split(f"({pattern})", chunk)
+
+    for cleaned_chunk in cleaned_chunks:
+        if cleaned_chunk in special_tokens:
+            # Don't tokenize special tokens
+            continue
+        for token in cleaned_chunk.split():
+        # for match in re.finditer(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""", cleaned_chunk):
+        #     token = match.group()
+            token_bytes = tuple(x.encode('utf-8') for x in token)
+            token_counts[token_bytes] = token_counts.get(token_bytes, 0) + 1
+
+    return token_counts
+
+
+def count_file_pre_tokens(filepath: str | os.PathLike, special_tokens: list[str]) -> dict[tuple[bytes, ...], int]:
+    """Split file contents into pre tokens and count them."""
     with open(filepath, "rb") as f:
         num_processes = 4
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
-        token_counts = {}
+        chunk_token_counts: list[dict] = []
 
-        # The following is a serial implementation, but you can parallelize this
-        # by sending each start/end pair to a set of processes.
         for start, end in zip(boundaries[:-1], boundaries[1:]):
             f.seek(start)
             chunk = f.read(end - start).decode("utf-8", errors="ignore")
-            # Run pre-tokenization on your chunk and store the counts for each pre-token
+            chunk_token_counts.append(count_chunk_pre_tokens(chunk, special_tokens))
+
+        combined_token_counts = {}
+        for token_counts in chunk_token_counts:
+            for token, count in token_counts.items():
+                combined_token_counts[token] = combined_token_counts.get(token, 0) + count
             
-            # for match in re.finditer(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""", chunk):
-            for token in chunk.split():
-                # token = match.group()
-                token_bytes = tuple(x.encode('utf-8') for x in token)
-                token_counts[token_bytes] = token_counts.get(token_bytes, 0) + 1
-            
-        return token_counts
+        return combined_token_counts
 
 
 def byte_tuple_to_str(token_tuple: tuple[bytes, ...]) -> str:
@@ -83,6 +104,8 @@ def token_dict_to_human_readable(token_dict: dict[tuple[bytes, ...], int]) -> di
 
 
 def count_byte_pairs(token_dict: dict[tuple[bytes, ...], int]) -> dict[tuple[bytes, bytes], int]:
+    """Given a token_dict, which contains pre-tokens and their counts, count number of byte
+    pairs."""
     pair_counts: dict[tuple[bytes, bytes], int] = {}
     for token_tuple, count in token_dict.items():
         for i in range(len(token_tuple) - 1):
@@ -92,6 +115,7 @@ def count_byte_pairs(token_dict: dict[tuple[bytes, ...], int]) -> dict[tuple[byt
 
 
 def join_pairs_in_token_tuple(token_tuple: tuple[bytes, ...], pair_to_join: tuple[bytes, bytes]) -> tuple[bytes, ...]:
+    """Given a tuple of bytes and a pair of bytes, join any adjacent bytes that match the byte pair."""
     token_list = []
     i = 0
     while i < len(token_tuple):
@@ -110,21 +134,53 @@ def join_pairs_in_token_dict(
 ) -> dict[tuple[bytes, ...], int]:
     return {join_pairs_in_token_tuple(k, most_common_pair): v
             for k, v in token_dict.items()}
-    
+
+
+def add_tokens_to_vocab(vocab: dict[int, bytes], tokens: list[bytes]) -> None:
+    for i, token in enumerate(tokens):
+        vocab[len(vocab) + i] = token
+
+
+def initial_vocabulary(special_tokens: list[str]) -> dict[int, bytes]:
+    vocab = {}
+    for i in range(256):
+        vocab[i] = bytes(i)
+        
+    add_tokens_to_vocab(vocab, [x.encode('utf-8') for x in special_tokens])
+
+    return vocab
+
+
+def train_bpe(
+    input_path: str | os.PathLike,
+    vocab_size: int,
+    special_tokens: list[str]
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    """BPE trainer, returns vocab dict and merge sequence."""
+    token_dict = count_file_pre_tokens(input_path, special_tokens)
+
+    merges = []
+    vocab = initial_vocabulary(special_tokens)
+    while len(vocab) < vocab_size:
+        # TODO optimize this to not iterate over bytes pairs every time
+        byte_pairs = count_byte_pairs(token_dict)
+        
+        if len(byte_pairs) == 0:
+            print('All pairs of bytes have been joined, exiting iteration early')
+            break
+        
+        most_common_pair = max(byte_pairs.items(), key=lambda x: (x[1], x[0]))[0]
+        merges.append(most_common_pair)
+        joined_pair = b''.join(most_common_pair)
+        add_tokens_to_vocab(vocab, [joined_pair])
+        
+        token_dict = join_pairs_in_token_dict(token_dict, most_common_pair)
+        
+    return vocab, merges
 
 
 if __name__ == "__main__":
-    token_dict = count_file_pre_tokens("data/bpe_training_example.txt")
-    print("Pre-token counts:")
-    print(token_dict)
+    train_bpe("data/bpe_training_example.txt", 512, ["<|endoftext|>"])
     
-    byte_pairs = count_byte_pairs(token_dict)
-    print("Byte pair counts:")
-    print(byte_pairs)
     
-    most_common_pair = max(byte_pairs.items(), key=lambda x: (x[1], x[0]))[0]
-    print("Most common byte pair:", byte_tuple_to_str(most_common_pair))
-    
-    joined_dict = join_pairs_in_token_dict(token_dict, most_common_pair)
-    print("Dict with pairs joined:", joined_dict)
     
